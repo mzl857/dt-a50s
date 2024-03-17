3a3184520c49e13deca07ecfd4ad7df0a6435a16
mzl857 <2602924930@qq.com>
2024-03-17 21:50:46 +0800

    upload device tree for a5070

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a50s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a50s-user \
+    omni_a50s-userdebug \
+    omni_a50s-eng
